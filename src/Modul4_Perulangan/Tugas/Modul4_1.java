4a147b7f9a767e6928c2a9bebb792481eb6f8c87
rulshrm <raflysahrul3114@gmail.com>
2025-01-03 11:43:34 +0700

    Implement diamond pattern in Modul4_1; adjust loop structure and spacing for output

--- src/Modul4_Perulangan/Tugas/Modul4_1.java
+++ src/Modul4_Perulangan/Tugas/Modul4_1.java
@@ -2,12 +2,29 @@ package Modul4_Perulangan.Tugas;
 
 public class Modul4_1 {
   public static void main(String[] args) {
-    int n = 5;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (i == j) {
-          System.out.print("*");
-        }
+    int n = 11;
+    // Bagian atas belah ketupat
+    for (int i = 1; i <= n; i++) {
+      // Cetak spasi
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
+      // Cetak bintang
+      for (int k = 1; k <= 2 * i - 1; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+    // Bagian bawah belah ketupat
+    for (int i = n-1; i >= 1; i--) {
+      // Cetak spasi
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
+      // Cetak bintang
+      for (int k = 1; k <= 2 * i - 1; k++) {
+        System.out.print("*");
       }
       System.out.println();
     }
